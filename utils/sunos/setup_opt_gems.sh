5849651d71eb1fded1f6ff97f875cd502752f681
Richard Spindler <richard@lateralblast.com.au>
2016-07-19 08:08:21 +1000

    Updated Solaris support

---
+++ utils/sunos/setup_opt_gems.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+gem install nokogiri -- --use-system-libraries
+gem install mechanize
+gem install net-ssh
+gem install net-scp
---
+++ utils/sunos/setup_req_gems.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+gem install getopt
+gem install builder
+gem install parseconfig
+gem install unix-crypt
+gem install netaddr
+gem install json
+gem install fileutils
